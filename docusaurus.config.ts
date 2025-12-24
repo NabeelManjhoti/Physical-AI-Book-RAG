6d27fa1aa83412f2eec7b7ce8f4065355a805a2f
NabeelManjhoti <nabeelalimanjhoi@gmail.com>
2025-12-25 01:13:44 +0500

    updated config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,8 +18,7 @@ const config: Config = {
   url: 'https://physical-ai-book-rag.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
-  trailingSlash: false,
+  baseUrl: "",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
