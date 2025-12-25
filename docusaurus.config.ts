0e87a4a0c410043f07294a96ab0e685f06bac03f
NabeelManjhoti <nabeelalimanjhoi@gmail.com>
2025-12-25 22:29:57 +0500

    docu config updated

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,13 +19,14 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-  trailingSlash: false,
   
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'NabeelManjhoti', // Usually your GitHub org/user name.
-  projectName: 'Physical-AI-Book-RAG', // Usually your repo name.
+  projectName: 'physical-ai-book-rag', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
